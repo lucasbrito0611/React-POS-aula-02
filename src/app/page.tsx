de50f643a7fb8ddafa85ccc67a0d545113ec9362
lucasbrito0611 <brito.s@escolar.ifrn.edu.br>
2025-04-24 10:36:35 -0300

    Tutorial até a prática 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,50 @@
 "use client";
 
+import React from "react";
+
+const Titulo = () => <h1 className="text-4xl font-bold mb-3">React - Conceitos Básicos</h1>
+
+const SubTitulo = () => (<h2 className="text-2xl font-bold mb-6">Lista de tarefas</h2>)
+
+const Cabecalho = () => (
+  <div className="text-center">
+    <Titulo />
+    <SubTitulo />
+  </div>
+)
+
+interface TarefaProps {
+  titulo: string;
+	concluido?: boolean;
+}
+
+class Tarefa extends React.Component<TarefaProps> {
+	render(): React.ReactNode {
+		const classe = `p-3 mb-3 rounded-lg shadow-md ${this.props.concluido ? "bg-gray-800" : "bg-gray-400"}`;
+
+		return (
+			<div className={classe}>
+				<h3 className="text-xl font-bold">{this.props.titulo}</h3>
+				<p className="text-sm">{this.props.concluido ? "Concluída" : "Pendente"}</p>
+			</div>
+		);
+	}
+}
+
 const Home = () => {
+  const tarefas = [
+		{ id: 1, title: "delectus aut autem", completed: false },
+		{ id: 2, title: "quis ut nam facilis et officia qui", completed: true },
+		{ id: 3, title: "fugiat veniam minus", completed: false },
+	];
+
   return (
-    <div>
-      <main>
-        <h1>React - conceitos básicos</h1>
-      </main>
+    <div className="container mx-auto p-4">
+      <Cabecalho />
+      <Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed} />
+			<Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed} />
     </div>
-  );
+  )
 }
 
 export default Home;
